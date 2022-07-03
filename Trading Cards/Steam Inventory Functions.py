6b36967b3189f4e3048dccbaae21aa43df2447dc
Rowan Dash <pokemonmaster9967@gmail.com>
2022-07-03 14:28:56 +0100

    Update Steam Inventory Functions.py

--- Trading Cards/Steam Inventory Functions.py
+++ Trading Cards/Steam Inventory Functions.py
@@ -1,23 +1,36 @@
 import requests as r
 import json
 
+
 def getInventory(id):
-    url = "https://steamcommunity.com/inventory/{}/753/6?l=english&count=5000".format(id)
+    url = "https://steamcommunity.com/inventory/{}/753/6?l=english&count=5000".format(
+        id)
     data = r.get(url)
     inv = data.json()
     with open("{}.json".format(str(id)), "w") as f:
         f.write(json.dumps(inv))
     f.close()
 
+
 def findDupes(id):
     with open("{}.json".format(str(id)), "r") as f:
         data = json.loads(f.read())
     f.close()
-    items = data["assets"]
+    items = data["descriptions"]
+    found = []
+    dupes = []
     for i in items:
-        if int(i["amount"]) > 1:
-            print(i)
+        if i["name"] in found:
+            dupes.append(i)
+            print(str(len(dupes)) + " found")
+        else:
+            found.append(i["name"])
 
-#getInventory(76561198261714500)
+    with open("{}_dupes.json".format(str(id)), "w") as f:
+        f.write(json.dumps(dupes))
+    f.close()
 
-findDupes(76561198261714500)
+
+# getInventory(76561198261714500)
+
+# findDupes(76561198261714500)
