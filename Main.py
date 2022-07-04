a570644eccd23eb93b46d2d3f8b44117d940ad6e
Rowan Dash <pokemonmaster9967@gmail.com>
2022-07-04 10:52:51 +0100

    Utilise login module

--- Main.py
+++ Main.py
@@ -1,9 +1,27 @@
 from Cleanup.file import setup, cleanup
 from Market.cards import *
+from Market.market import *
+from Login.login import *
+import requests as r
 import time
 
-setup(76561198261714500)
-getInventory(76561198261714500)
-findDupes(76561198261714500)
+username = input("Enter your Steam username: ")
+password = input("Enter your Steam password: ")
+jar = r.cookies.RequestsCookieJar()
+
+auth_ctx = login(jar, username, password)
+print(auth_ctx)
+
+id = auth_ctx['steamid']
+
+# init session
+resp = r.get('https://steamcommunity.com/', cookies=jar)
+jar.update(resp.cookies)
+
+market_jar = transfer_login(jar, auth_ctx)
+
+setup(id)
+getInventory(id)
+findDupes(id)
 time.sleep(5)
-cleanup(76561198261714500)
+cleanup(id)
