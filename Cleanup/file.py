8995f78f66a2de39317a66946367c8b6c5bc53b3
Rowan Dash <pokemonmaster9967@gmail.com>
2022-07-04 10:42:29 +0100

    Create file.py

---
+++ Cleanup/file.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+
+def setup(id):
+    if os.path.exists(str(id)):
+        pass
+    else:
+        os.makedirs(str(id))
+
+
+def cleanup(file_path):
+    # delete the directory
+    try:
+        shutil.rmtree(str(file_path))
+    except:
+        pass
